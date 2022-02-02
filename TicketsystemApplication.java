7ff1c9bf8c9c8611bf05fdc84234d3bca6b23247
anushakr1234 <97779982+anushakr1234@users.noreply.github.com>
2022-02-02 12:53:09 +0530

    Add files via upload

---
+++ TicketsystemApplication.java
@@ -0,0 +1,13 @@
+package com.team.java.ticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsystemApplication.class, args);
+	}
+
+}
